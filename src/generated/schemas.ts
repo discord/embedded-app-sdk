b69bc49f604d64e1b18078d747da7114d2911cc2
Matt Hova <matthova87@gmail.com>
2024-03-11 12:26:45 -0700

    remove strict

--- src/generated/schemas.ts
+++ src/generated/schemas.ts
@@ -7,107 +7,97 @@ import {z, infer as zInfer} from 'zod';
 import {fallbackToDefault} from '../utils/zodUtils';
 
 // INITIATE_IMAGE_UPLOAD
-export const InitiateImageUploadResponseSchema = z.object({image_url: z.string()}).strict();
+export const InitiateImageUploadResponseSchema = z.object({image_url: z.string()});
 export type InitiateImageUploadResponse = zInfer<typeof InitiateImageUploadResponseSchema>;
 
 // OPEN_SHARE_MOMENT_DIALOG
-export const OpenShareMomentDialogRequestSchema = z.object({mediaUrl: z.string().max(1024)}).strict();
+export const OpenShareMomentDialogRequestSchema = z.object({mediaUrl: z.string().max(1024)});
 export type OpenShareMomentDialogRequest = zInfer<typeof OpenShareMomentDialogRequestSchema>;
 
 // AUTHENTICATE
-export const AuthenticateRequestSchema = z.object({access_token: z.union([z.string(), z.null()]).optional()}).strict();
+export const AuthenticateRequestSchema = z.object({access_token: z.union([z.string(), z.null()]).optional()});
 export type AuthenticateRequest = zInfer<typeof AuthenticateRequestSchema>;
-export const AuthenticateResponseSchema = z
-  .object({
-    access_token: z.string(),
-    user: z
-      .object({
-        username: z.string(),
-        discriminator: z.string(),
-        id: z.string(),
-        avatar: z.union([z.string(), z.null()]).optional(),
-        public_flags: z.number(),
-        global_name: z.union([z.string(), z.null()]).optional(),
-      })
-      .strict(),
-    scopes: z.array(
-      fallbackToDefault(
-        z
-          .enum([
-            'identify',
-            'email',
-            'connections',
-            'guilds',
-            'guilds.join',
-            'guilds.members.read',
-            'gdm.join',
-            'rpc',
-            'rpc.notifications.read',
-            'rpc.voice.read',
-            'rpc.voice.write',
-            'rpc.video.read',
-            'rpc.video.write',
-            'rpc.screenshare.read',
-            'rpc.screenshare.write',
-            'rpc.activities.write',
-            'bot',
-            'webhook.incoming',
-            'messages.read',
-            'applications.builds.upload',
-            'applications.builds.read',
-            'applications.commands',
-            'applications.commands.update',
-            'applications.commands.permissions.update',
-            'applications.store.update',
-            'applications.entitlements',
-            'activities.read',
-            'activities.write',
-            'relationships.read',
-            'voice',
-            'dm_channels.read',
-            'role_connections.write',
-          ])
-          .or(z.literal(-1))
-          .default(-1)
-      )
-    ),
-    expires: z.string(),
-    application: z
-      .object({
-        description: z.string(),
-        icon: z.union([z.string(), z.null()]).optional(),
-        id: z.string(),
-        rpc_origins: z.array(z.string()).optional(),
-        name: z.string(),
-      })
-      .strict(),
-  })
-  .strict();
+export const AuthenticateResponseSchema = z.object({
+  access_token: z.string(),
+  user: z.object({
+    username: z.string(),
+    discriminator: z.string(),
+    id: z.string(),
+    avatar: z.union([z.string(), z.null()]).optional(),
+    public_flags: z.number(),
+    global_name: z.union([z.string(), z.null()]).optional(),
+  }),
+  scopes: z.array(
+    fallbackToDefault(
+      z
+        .enum([
+          'identify',
+          'email',
+          'connections',
+          'guilds',
+          'guilds.join',
+          'guilds.members.read',
+          'gdm.join',
+          'rpc',
+          'rpc.notifications.read',
+          'rpc.voice.read',
+          'rpc.voice.write',
+          'rpc.video.read',
+          'rpc.video.write',
+          'rpc.screenshare.read',
+          'rpc.screenshare.write',
+          'rpc.activities.write',
+          'bot',
+          'webhook.incoming',
+          'messages.read',
+          'applications.builds.upload',
+          'applications.builds.read',
+          'applications.commands',
+          'applications.commands.update',
+          'applications.commands.permissions.update',
+          'applications.store.update',
+          'applications.entitlements',
+          'activities.read',
+          'activities.write',
+          'relationships.read',
+          'voice',
+          'dm_channels.read',
+          'role_connections.write',
+        ])
+        .or(z.literal(-1))
+        .default(-1)
+    )
+  ),
+  expires: z.string(),
+  application: z.object({
+    description: z.string(),
+    icon: z.union([z.string(), z.null()]).optional(),
+    id: z.string(),
+    rpc_origins: z.array(z.string()).optional(),
+    name: z.string(),
+  }),
+});
 export type AuthenticateResponse = zInfer<typeof AuthenticateResponseSchema>;
 
 // GET_ACTIVITY_INSTANCE_CONNECTED_PARTICIPANTS
-export const GetActivityInstanceConnectedParticipantsResponseSchema = z
-  .object({
-    participants: z.array(
-      z
-        .object({
-          id: z.string(),
-          username: z.string(),
-          global_name: z.union([z.string(), z.null()]).optional(),
-          discriminator: z.string(),
-          avatar: z.union([z.string(), z.null()]).optional(),
-          flags: z.number(),
-          bot: z.boolean(),
-          avatar_decoration_data: z
-            .union([z.object({asset: z.string(), skuId: z.string().optional()}).strict(), z.null()])
-            .optional(),
-          premium_type: z.union([z.number(), z.null()]).optional(),
-          nickname: z.string().optional(),
-        })
-        .strict()
-    ),
-  })
-  .strict();
+export const GetActivityInstanceConnectedParticipantsResponseSchema = z.object({
+  participants: z.array(
+    z.object({
+      id: z.string(),
+      username: z.string(),
+      global_name: z.union([z.string(), z.null()]).optional(),
+      discriminator: z.string(),
+      avatar: z.union([z.string(), z.null()]).optional(),
+      flags: z.number(),
+      bot: z.boolean(),
+      avatar_decoration_data: z
+        .union([z.object({asset: z.string(), skuId: z.string().optional()}), z.null()])
+        .optional(),
+      premium_type: z.union([z.number(), z.null()]).optional(),
+      nickname: z.string().optional(),
+    })
+  ),
+});
 export type GetActivityInstanceConnectedParticipantsResponse = zInfer<
   typeof GetActivityInstanceConnectedParticipantsResponseSchema
 >;
