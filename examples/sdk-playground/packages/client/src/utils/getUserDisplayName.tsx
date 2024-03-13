aa1a39cede595a38d85ecfb2b090e8a012283c53
Matt Hova <matthova87@gmail.com>
2024-03-12 20:47:35 -0700

    Not null-checking

--- examples/sdk-playground/packages/client/src/utils/getUserDisplayName.tsx
+++ examples/sdk-playground/packages/client/src/utils/getUserDisplayName.tsx
@@ -9,7 +9,7 @@ interface GetUserDisplayNameArgs {
 export function getUserDisplayName({guildMember, user}: GetUserDisplayNameArgs) {
   if (guildMember?.nick != null && guildMember.nick !== '') return guildMember.nick;
 
-  if (user.discriminator != '0') return `${user.username}#${user.discriminator}`;
+  if (user.discriminator !== '0') return `${user.username}#${user.discriminator}`;
 
   if (user.global_name != null && user.global_name !== '') return user.global_name;
 
