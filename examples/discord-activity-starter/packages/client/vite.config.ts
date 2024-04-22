c22c7724bc76ac2408c1cd01a9bb0b6992b724c7
Justin Beckwith <justin.beckwith@gmail.com>
2024-04-22 09:45:57 -0700

    chore: update samples to vite 5.x (#143)

--- examples/discord-activity-starter/packages/client/vite.config.ts
+++ examples/discord-activity-starter/packages/client/vite.config.ts
@@ -4,6 +4,7 @@ import {defineConfig} from 'vite';
 export default defineConfig({
   envDir: '../../',
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
--- examples/react-colyseus/packages/client/vite.config.ts
+++ examples/react-colyseus/packages/client/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   plugins: [react()],
   envDir: '../../',
   server: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
